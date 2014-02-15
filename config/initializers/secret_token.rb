b2aa0bdf76772ecb754fa91015a1c02c50168f4d
Van Michael <van.michael.nguyen@gmail.com>
2014-02-15 15:37:27 -0500

    first commit with gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VannDiagram::Application.config.secret_key_base = '7db41ba36b86c5ae2520ca6a98b6f0b8c870a810d4afa480f127116736c6feb659fddfe32608550aae3e71a5d952ce9b5a02c9cdd84c1f3ce5ca83e5d51d60b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VannDiagram::Application.config.session_store :cookie_store, key: '_vann_diagram_session'
