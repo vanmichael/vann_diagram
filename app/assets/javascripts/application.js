a1827b1aed188593b5cbe0365e391302a9997f6c
Van Michael <van.michael.nguyen@gmail.com>
2014-02-15 15:59:50 -0500

    Install Ember Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,18 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require vann_diagram
+
+// for more details see: http://emberjs.com/guides/application/
+VannDiagram = Ember.Application.create();
+
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
