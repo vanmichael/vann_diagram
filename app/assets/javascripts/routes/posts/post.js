ac4d37b2cd05415ec962926a46085205b799374f
Van Michael <van.michael.nguyen@gmail.com>
2014-02-15 20:29:02 -0500

    refactor out static data

--- app/assets/javascripts/routes/posts/post.js
+++ app/assets/javascripts/routes/posts/post.js
@@ -3,18 +3,3 @@ VannDiagram.PostsPostRoute = Ember.Route.extend({
     return posts.findBy('id', params.post_id);
   }
 });
-
-var posts = [
-  {
-    id: 1,
-    title: "Van's First Post",
-    author: { name: "van" },
-    body: "My first ember post."
-  },
-  {
-    id: 2,
-    title: "Van's Second Post",
-    author: { name: "van" },
-    body: "My second ember post."
-  }
-];
